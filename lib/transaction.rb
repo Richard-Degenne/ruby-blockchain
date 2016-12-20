6934581e0b0c1a4808d377b1235c94c3c4f43369
Richard-Degenne <richdeg2@gmail.com>
2016-12-20 12:27:53 +0100

    Added `total_input`, `total_output`
    Also wrote unit tests for these methods.

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,17 +1,32 @@
-require 'date'
-
 class Transaction
-    attr_reader: @hash
-    attr_reader: @index
-    attr_reader: @date
-    attr_reader: @inputs
-    attr_reader: @outputs
+    ## Transaction hash, as a string
+    attr_reader :hash
+
+    ## Transaction index, as an integer
+    attr_reader :index
+
+    ## Transaction date, as a Datetime object
+    attr_reader :date
+
+    ## Bitcoin inputs, as an integer list
+    attr_reader :inputs
+
+    ## Bitcoin outputs, as an interger list
+    attr_reader :outputs
 
     def initialize(hash, index, date, inputs, outputs)
         @hash = hash
         @index = index
-        @date = Time.at(date).to_datetime
+        @date = date
         @inputs = inputs
         @outputs = outputs
+    end
+
+    def total_input
+        @inputs.inject(:+)
+    end
+
+    def total_output
+        @outputs.inject(:+)
     end
 end
