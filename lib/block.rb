0d02c98c5c93449b15e3637a414bfa1b01a019da
Richard-Degenne <richdeg2@gmail.com>
2016-12-20 12:33:11 +0100

    Fixed `Block`

--- lib/block.rb
+++ lib/block.rb
@@ -1,13 +1,11 @@
-require 'date'
-
 class Block
-    attr_reader: @hash
-    attr_reader: @date
-    attr_reader: @transactions
+    attr_reader :hash
+    attr_reader :date
+    attr_reader :transactions
 
     def initialize(hash, date, transactions)
         @hash = hash
-        @date = Time.at(date).to_datetime
+        @date = date
         @transactions = transactions
     end
 end
